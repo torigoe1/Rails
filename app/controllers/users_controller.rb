c9f98e4f575e9361a341bebaab79dd32ff8eedb3
鳥越瑛裕 <admin@user.local>
2023-09-02 15:51:34 +0900

    機能完成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,14 +16,28 @@ class UsersController < ApplicationController
   end
 
   def show
+    @post = Post.find_by(id:params[:id])
   end
 
   def edit
+    @post = Post.find_by(id:params[:id])
   end
 
   def update
+    @post = Post.find_by(id:params[:id])
+    @post.title = params[:title]
+    @post.start = params[:start]
+    @post.end = params[:end]
+    @post.all = params[:all]
+    @post.memo = params[:memo]
+    @post.save
+
+    redirect_to("/posts")
   end
 
   def destroy
+    @post = Post.find_by(id:paramd[:id])
+    @post.destroy
+    redirect_to("/posts")
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   get 'users/index'
   get 'posts',to:'posts#post'
   get 'posts/new',to:'users#new'
+  get 'posts/:id',to:'users#show'
   post 'posts/create',to:'users#create'
+  get 'posts/:id/edit',to:'users#edit'
+  post 'posts/:id/update',to:'users#update'
+  post 'posts/:id/destroy',to:'users#destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
