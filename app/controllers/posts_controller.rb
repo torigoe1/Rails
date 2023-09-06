a39993d4c689d4f4a6b4699ce9917e8eb958c755
torigoe1 <jfw.dia.wss1945@gmail.com>
2023-09-06 19:19:20 +0900

    修正

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,56 @@
 class PostsController < ApplicationController
-  def post
+  protect_from_forgery
+  def index
     @posts = Post.all
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(title: params[:title],
+                     start: params[:start],
+                     end: params[:end],
+                     all: params[:all],
+                     memo: params[:memo]) 
+    if @post.save
+      flash[:notice] = "スケジュールを登録しました"
+      redirect_to posts_path
+    else
+      flash[:notice] = "スケジュール登録に失敗しました"
+      render action: :new
+    end
+  end
+
+  def show
+    @post = Post.find_by(id:params[:id])
+  end
+
+  def edit
+    @post = Post.find_by(id:params[:id])
+  end
+
+  def update
+    @post = Post.find_by(id:params[:id])
+    @post.title = params[:title]
+    @post.start = params[:start]
+    @post.end = params[:end]
+    @post.all = params[:all]
+    @post.memo = params[:memo]
+    if @post.save
+      flash[:notice] = "スケジュールを登録しました"
+      redirect_to posts_path
+    else
+      flash[:notice] = "スケジュール登録に失敗しました"
+      render action: :edit
+    end
+  end
+
+  def destroy
+    @post = Post.find_by(id:params[:id])
+    @post.destroy
+    redirect_to posts_path
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,4 @@
 Rails.application.routes.draw do
-  get 'users/index'
-  get 'posts',to:'posts#post'
-  get 'posts/new',to:'users#new'
-  get 'posts/:id',to:'users#show'
-  post 'posts/create',to:'users#create'
-  get 'posts/:id/edit',to:'users#edit'
-  post 'posts/:id/update',to:'users#update'
-  post 'posts/:id/destroy',to:'users#destroy'
+  resources :posts
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
